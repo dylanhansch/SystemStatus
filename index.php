8716fbd729e8ea52da55590fc0cd09f0b4da2447
Dylan Hansch <dylan@dylanhansch.net>
2014-06-13 13:13:57 -0500

    Bootsrap and Basic Index
    No functionality yet, just styling and template. Getting ready to make
    this project come to life!

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>System Status</title>
+		<meta charset="utf-8">
+		<meta name="author" content="Dylan Hansch">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="">System Status</a>
+				</div>
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav pull-left">
+						
+					</ul>
+				</div>
+			</div>
+		</nav>
+		
+		
+		
+		<hr>
+		<div id="footer">
+			<div class="container">
+				<p class="text-muted" align="center"><a href="//github.com/dylanhansch/SystemStatus">System Status</a> by <a href="//dylanhansch.net">Dylan Hansch</a></p>
+			</div>
+		</div>		
+	</body>
+</html>
