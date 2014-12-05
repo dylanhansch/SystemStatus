98880f1eb8f1a3e48baf813d51da67ff2b27e67d
Dylan Hansch <dylan@dylanhansch.net>
2014-12-04 23:11:47 -0600

    Fetch data
    Temporarily using load average. Will soon be a percentage :)

--- status/index.php
+++ status/index.php
@@ -5,18 +5,29 @@ require_once("global.php");
 function servers(){
 	global $mysqli;
 	
-	$stmt = $mysqli->prepare("SELECT name,location,host,type FROM servers ORDER BY id");
+	$stmt = $mysqli->prepare("SELECT name,location,host,type,url FROM servers ORDER BY id");
 	echo($mysqli->error);
 	$stmt->execute();
-	$stmt->bind_result($out_name,$out_location,$out_host,$out_type);
+	$stmt->bind_result($out_name,$out_location,$out_host,$out_type,$out_url);
 	$servers = array();
 	
 	while($stmt->fetch()){
-		$servers[] = array('name' => $out_name, 'location' => $out_location, 'host' => $out_host, 'type' => $out_type);
+		$servers[] = array('name' => $out_name, 'location' => $out_location, 'host' => $out_host, 'type' => $out_type, 'url' => $out_url);
 	}
 	$stmt->close();
 	
 	return $servers;
+}
+
+function get_data($url){
+  $ch = curl_init();
+  $timeout = 5;
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
 }
 ?>
 <!DOCTYPE html>
@@ -52,17 +63,51 @@ function servers(){
 								<th>HDD (Free)</th>
 							</tr>
 							<?php $servers = servers();
-							foreach($servers as $server): ?>
+							foreach($servers as $server):
+							
+							$output = get_data($server['url']);
+							if(($output == NULL) || ($output === false)){
+								$data = array();
+								$data['uptime'] = '
+								<div class="progress">
+									<div class="progress-bar progress-bar-danger" role="progressbar" style="width: 100%;"><small>Down</small></div>
+								</div>
+								';
+								$data['load'] = '
+								<div class="progress">
+									<div class="progress-bar progress-bar-danger" role="progressbar" style="width: 100%;"><small>Down</small></div>
+								</div>
+								';
+								$data['online'] = '
+								<div class="progress">
+									<div class="progress-bar progress-bar-danger" role="progressbar" style="width: 100%;"><small>Down</small></div>
+								</div>
+								';
+								$data['memory'] = '
+								<div class="progress progress-striped active">
+									<div class="progress-bar progress-bar-danger" role="progressbar" style="width: 100%;"><small>n/a</small></div>
+								</div>
+								';
+								$data['hdd'] = '
+								<div class="progress progress-striped active">
+									<div class="progress-bar progress-bar-danger" role="progressbar" style="width: 100%;"><small>n/a</small></div>
+								</div>
+								';
+							}else{
+								$data = json_decode($output, true);
+								$data["load"] = number_format($data["load"], 2);
+							}
+							?>
 							<tr>
-								<td></td>
+								<td><?php echo($data['online']); ?></td>
 								<td><?php echo($server['name']); ?></td>
 								<td><?php echo($server['type']); ?></td>
 								<td><?php echo($server['host']); ?></td>
 								<td><?php echo($server['location']); ?></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td><?php echo($data['uptime']); ?></td>
+								<td><?php echo($data['load']); ?></td>
+								<td><?php echo($data['memory']); ?></td>
+								<td><?php echo($data['hdd']); ?></td>
 							</tr>
 							<?php endforeach; ?>
 						</table>
--- status/remote.php
+++ status/remote.php
@@ -1,3 +1,69 @@
 <?php
 // Data setup
 $array = array();
+
+// Change Unix timestamp to Human timestamp
+function sec2human($time) {
+  $seconds = $time%60;
+	$mins = floor($time/60)%60;
+	$hours = floor($time/60/60)%24;
+	$days = floor($time/60/60/24);
+	return $days > 0 ? $days . ' day'.($days > 1 ? 's' : '') : $hours.':'.$mins.':'.$seconds;
+}
+
+// System Uptime
+$fh = fopen('/proc/uptime', 'r');
+$uptime = fgets($fh);
+fclose($fh);
+$uptime = explode('.', $uptime, 2);
+$array['uptime'] = sec2human($uptime[0]);
+
+// Memory (RAM)
+$fh = fopen('/proc/meminfo', 'r');
+  $mem = 0;
+  while ($line = fgets($fh)) {
+    $pieces = array();
+    if (preg_match('/^MemTotal:\s+(\d+)\skB$/', $line, $pieces)) {
+      $memoryT = $pieces[1];
+    }
+    if (preg_match('/^MemFree:\s+(\d+)\skB$/', $line, $pieces)) {
+      $memoryF = $pieces[1];
+    }
+    if (preg_match('/^Cached:\s+(\d+)\skB$/', $line, $pieces)) {
+      $memoryC = $pieces[1];
+      break;
+    }
+  }
+fclose($fh);
+$memoryM = $memoryC + $memoryF;
+$memoryM2 = $memoryM / $memoryT * 100;
+$memory = round($memoryM2) . '%';
+if ($memory >= "51%") { $memoryL = "success"; }
+elseif ($memory <= "50%") { $memoryL = "warning"; }
+elseif ($memory <= "35%") { $memoryL = "danger"; }
+$array['memory'] = '<div class="progress progress-striped active">
+<div class="progress-bar progress-bar-'.$memoryL.'" role="progressbar" style="width: '.$memory.';">'.$memory.'</div>
+</div>';
+
+// Storage (Disk)
+$storageT = disk_total_space("/");
+$storageF = disk_free_space("/");
+$storageM = $storageF / $storageT * 100;
+$storage = round($storageM) . '%';
+if ($storage >= "51%") { $storageL = "success"; }
+elseif ($storage <= "50%") { $storageL = "warning"; }
+elseif ($storage <= "35%") { $storageL = "danger"; }
+$array['hdd'] = '<div class="progress progress-striped active">
+<div class="progress-bar progress-bar-'.$storageL.'" role="progressbar" style="width: '.$storage.';">'.$storage.'</div>
+</div>';
+
+// Temporary Load Average
+$load = sys_getloadavg();
+$array['load'] = $load[0];
+
+// If the server is online
+$array['online'] = '<div class="progress">
+<div class="progress-bar progress-bar-success" role="progressbar" style="width: 100%;"><small>Up</small></div>
+</div>';
+
+echo json_encode($array);
