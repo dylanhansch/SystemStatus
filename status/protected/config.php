48148a3e5c16abbeb48f43c5cfcefa3384c5bae5
Dylan Hansch <dylan@dylanhansch.net>
2014-12-08 11:20:21 -0600

    Email notification cron job

--- status/protected/config.php
+++ status/protected/config.php
@@ -2,12 +2,27 @@
 // Basic Customization
 $refresh = "5000";
 $basedir = "/status/";
+$support_url = "http://example.com/support";
+
+// Settings for Cron
+$app_url = "http://example.com/status";
+$smtp = "example.com";
+$smtp_user = "user";
+$smtp_pass = "pass";
+$smtp_secure = "tls";
+$smtp_port = 587;
+$sender = "info@example.com";
+$sender_name = "example";
+$replyto = "info@example.com";
+$replyto_name = "example";
+$contact = "user@example.com";
+$contact_name = "example user";
 
 // Connection
 $host = 'localhost';
 $user = 'demo';
 $pass = '';
-$database = 'systemstatus';
+$database = 'systemstatusdev';
 $mysqli = new mysqli($host, $user, $pass, $database);
 if ($mysqli->connect_errno) {
     die('Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
