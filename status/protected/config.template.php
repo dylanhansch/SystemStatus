2f27769aa7c588bbba06ae67846a423a2180470d
Dylan Hansch <dylan@dylanhansch.net>
2016-06-11 20:21:03 -0500

    Remove live config

--- status/protected/config.template.php
+++ status/protected/config.template.php
@@ -2,13 +2,15 @@
 // Basic Customization
 $refresh = "6000";
 $basedir = "/status/";
+$baseurl = "http://example.com/status/";
+$company = "My Company";
 $support_url = "http://example.com/support";
 
 // Connection
 $host = 'localhost';
-$user = 'demo';
-$pass = '';
-$database = 'systemstatusdev';
+$user = 'systemstatus';
+$pass = '123456';
+$database = 'systemstatus';
 $mysqli = new mysqli($host, $user, $pass, $database);
 if ($mysqli->connect_errno) {
     die('Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
